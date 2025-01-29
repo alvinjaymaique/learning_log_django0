a44da0aa0392237a093abe2031a7ff13f4b63e7c
alvinjaymaique <alvinjay.maique.56@gmail.com>
2025-01-29 19:43:29 +0800

    added templates, url navigation, models, views

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -31,12 +31,17 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # My apps
+    'learning_logs',
+    
+    # Django default apps
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
 ]
 
 MIDDLEWARE = [
